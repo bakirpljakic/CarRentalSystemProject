91c21c5dbfc8b9587b2d0d35f6c1f289ffd484a5
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2022-12-19 15:40:39 +0100

    Added Cars class

--- src/main/java/ba/unsa/etf/rpr/domain/Cars.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Cars.java
@@ -1,4 +1,126 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Cars {
+    private int id;
+    private String make;
+    private  String model;
+    private int year;
+    private String category;
+    private boolean airconditioner;
+    private boolean navigation;
+    private boolean abs;
+    private int price;
+    private boolean available;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isAirconditioner() {
+        return airconditioner;
+    }
+
+    public void setAirconditioner(boolean airconditioner) {
+        this.airconditioner = airconditioner;
+    }
+
+    public boolean isNavigation() {
+        return navigation;
+    }
+
+    public void setNavigation(boolean navigation) {
+        this.navigation = navigation;
+    }
+
+    public boolean isAbs() {
+        return abs;
+    }
+
+    public void setAbs(boolean abs) {
+        this.abs = abs;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars = (Cars) o;
+        return id == cars.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, make, model, year, category, airconditioner, navigation, abs, price, available);
+    }
+
+    @Override
+    public String toString() {
+        return "Cars{" +
+                "id=" + id +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", category='" + category + '\'' +
+                ", airconditioner=" + airconditioner +
+                ", navigation=" + navigation +
+                ", abs=" + abs +
+                ", price=" + price +
+                ", available=" + available +
+                '}';
+    }
 }
