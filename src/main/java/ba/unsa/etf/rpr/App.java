aa6d6634f668dbfd20c87e0adba679e939768702
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2022-12-25 20:22:44 +0100

    Updated login implementation

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,31 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import ba.unsa.etf.rpr.Controllers.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+       /* Controller controller = new Controller();
+        fxmlLoader.setController(controller);*/
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        stage.setTitle("LOGIN");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/ba/unsa/etf/rpr/Controllers/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Controller.java
@@ -1,9 +1,25 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.App;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.Objects;
+
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class Controller {
+    public Controller() {
+    }
 
     public Label welcomeText;
 
@@ -13,5 +29,18 @@ public class Controller {
 
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void buttonClick(javafx.event.ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/registracija.fxml"));
+       // Registracija r = fxmlLoader.getController();
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        //stage.setTitle("");
+        //stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
