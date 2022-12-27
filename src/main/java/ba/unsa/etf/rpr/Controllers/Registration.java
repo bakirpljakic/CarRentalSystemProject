1d95b650bb26d12cb6f4cfb431f8fdd340535da2
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2022-12-27 11:20:46 +0100

    Added registration class

---
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Registration.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Registration {
+    public TextField KorisnikID;
+    public TextField EmailID;
+    public PasswordField LozinkaID;
+    public TextField GradID;
+    public TextField AdresaID;
+    public TextField BrVozackeID;
+
+    public Registration(){
+
+    }
+    @FXML
+    public void initialize() {
+
+    }
+}
