29b5e7d15df639fc0d0f925fead7aab21025e631
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-09 22:48:29 +0100

    Added save option

---
+++ src/main/java/ba/unsa/etf/rpr/Controllers/CarsController.java
@@ -0,0 +1,53 @@
+package ba.unsa.etf.rpr.Controllers;
+
+
+import ba.unsa.etf.rpr.dao.CarsDao;
+import ba.unsa.etf.rpr.dao.CarsDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Cars;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class CarsController {
+
+
+
+    public TextField MarkaID;
+    public TextField ModelID;
+    public TextField GodisteID;
+    @FXML
+    public ChoiceBox<String> DostupnoID;
+    public TextField CIjenaID;
+
+    Cars c = new Cars();
+    CarsDao carsDao = new CarsDaoSQLImpl();
+
+    @FXML
+    public void initialize() {
+    }
+
+    public void save(ActionEvent actionEvent) {
+        String marka = MarkaID.getText();
+        String model = ModelID.getText();
+        int godina = Integer.parseInt(GodisteID.getText());
+        int cijena = Integer.parseInt(CIjenaID.getText());
+        boolean dostupno;
+        String choice = DostupnoID.getValue();
+        if(choice.equals("DA")){
+            dostupno = true;
+        }else{
+            dostupno = false;
+        }
+        c = new Cars(0,marka,model, godina, cijena, dostupno);
+        carsDao.add(c);
+
+
+    }
+
+    public void change(ActionEvent actionEvent) {
+    }
+
+    public void delete(ActionEvent actionEvent) {
+    }
+}
