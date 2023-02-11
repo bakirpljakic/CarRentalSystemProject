19b99163fc283bee4b17e0e7d8e72c335cb33780
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-11 19:39:03 +0100

    Added exceptions

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.CarsException;
+
 import java.util.List;
 
 public interface Dao<T>{
-    T getById(int id);
-    T add(T item);
-    T update(T item);
-    void delete(int item);
-    List<T> getAll();
+    T getById(int id) throws CarsException;
+    T add(T item) throws CarsException;
+    T update(T item) throws  CarsException;
+    void delete(int id) throws CarsException;
+    List<T> getAll() throws CarsException;
 }
