7520989422a9c2a9022397a520a500297f67b865
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-11 18:51:02 +0100

    Added AbstractDao implementation

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,4 +1,180 @@
 package ba.unsa.etf.rpr.dao;
 
-public class AbstractDao {
+import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.CarsException;
+import java.sql.*;
+import java.util.*;
+
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private static Connection connection = null;
+    private String tableName;
+
+    public AbstractDao(String tableName) {
+        this.tableName = tableName;
+        createConnection();
+    }
+
+    private static void createConnection() {
+        if (AbstractDao.connection == null) {
+            String server = new String();
+            String user = new String();
+            String pass = new String();
+            try {
+                Properties prop = new Properties();
+                prop.load(ClassLoader.getSystemResource("db.properties").openStream());
+                server = prop.getProperty("db.url");
+                user = prop.getProperty("db.user");
+                pass = prop.getProperty("db.password");
+                AbstractDao.connection = DriverManager.getConnection(server, user, pass);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Connection getConnection() {
+        return AbstractDao.connection;
+    }
+    public abstract T row2object(ResultSet rs) throws CarsException;
+
+
+    public abstract Map<String, Object> object2row(T object);
+
+    public List<T> executeQuery(String query, Object[] params) throws CarsException{
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            if (params != null) {
+                for (int i = 1; i <= params.length; i++) {
+                    stmt.setObject(i, params[i - 1]);
+                }
+            }
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<T> resultList = new ArrayList<>();
+            while (rs.next()) {
+                resultList.add(row2object(rs));
+            }
+            return resultList;
+        } catch (SQLException e) {
+            throw new CarsException(e.getMessage(), e);
+        }
+    }
+
+   public T executeQueryUnique(String query, Object[] params) throws CarsException {
+       List<T> result = executeQuery(query, params);
+       if (result != null && result.size() == 1) {
+           return result.get(0);
+       } else {
+           throw new CarsException("Object not found");
+       }
+   }
+
+
+    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row) {
+        StringBuilder columns = new StringBuilder();
+        StringBuilder questions = new StringBuilder();
+
+        int counter = 0;
+        for (Map.Entry<String, Object> entry : row.entrySet()) {
+            counter++;
+            if (entry.getKey().equals("id")) continue; //skip insertion of id due autoincrement
+            columns.append(entry.getKey());
+            questions.append("?");
+            if (row.size() != counter) {
+                columns.append(",");
+                questions.append(",");
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(columns.toString(), questions.toString());
+    }
+
+
+    private String prepareUpdateParts(Map<String, Object> row) {
+        StringBuilder columns = new StringBuilder();
+
+        int counter = 0;
+        for (Map.Entry<String, Object> entry : row.entrySet()) {
+            counter++;
+            if (entry.getKey().equals("id")) continue; //skip update of id due where clause
+            columns.append(entry.getKey()).append("= ?");
+            if (row.size() != counter) {
+                columns.append(",");
+            }
+        }
+        return columns.toString();
+    }
+
+    public T getById(int id) throws CarsException {
+        return executeQueryUnique("SELECT * FROM " + this.tableName + " WHERE id = ?", new Object[]{id});
+    }
+
+    public List<T> getAll() throws CarsException {
+        return executeQuery("SELECT * FROM " + tableName, null);
+    }
+
+    public void delete(int id) throws CarsException{
+        String sql = "DELETE FROM " + tableName + " WHERE id = ?";
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new CarsException(e.getMessage(), e);
+        }
+    }
+
+    public T add(T item) throws CarsException{
+        Map<String, Object> row = object2row(item);
+        Map.Entry<String, String> columns = prepareInsertParts(row);
+        StringBuilder builder = new StringBuilder();
+        builder.append("INSERT INTO ").append(tableName);
+        builder.append(" (").append(columns.getKey()).append(") ");
+        builder.append("VALUES (").append(columns.getValue()).append(")");
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(builder.toString(), Statement.RETURN_GENERATED_KEYS);
+            // bind params. IMPORTANT treeMap is used to keep columns sorted so params are bind correctly
+            int counter = 1;
+            for (Map.Entry<String, Object> entry : row.entrySet()) {
+                if (entry.getKey().equals("id")) continue; // skip ID
+                stmt.setObject(counter, entry.getValue());
+                counter++;
+            }
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next(); // we know that there is one key
+            item.setId(rs.getInt(1)); //set id to return it back */
+            return item;
+        } catch (SQLException e) {
+            throw new CarsException(e.getMessage(), e);
+        }
+    }
+
+    public T update(T item) throws CarsException {
+        Map<String, Object> row = object2row(item);
+        String updateColumns = prepareUpdateParts(row);
+        StringBuilder builder = new StringBuilder();
+        builder.append("UPDATE ")
+                .append(tableName)
+                .append(" SET ")
+                .append(updateColumns)
+                .append(" WHERE id = ?");
+
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(builder.toString());
+            int counter = 1;
+            for (Map.Entry<String, Object> entry : row.entrySet()) {
+                if (entry.getKey().equals("id")) continue; // skip ID
+                stmt.setObject(counter, entry.getValue());
+                counter++;
+            }
+            stmt.setObject(counter, item.getId());
+            stmt.executeUpdate();
+            return item;
+        } catch (SQLException e) {
+            throw new CarsException(e.getMessage(), e);
+        }
+    }
 }
+
+
+
