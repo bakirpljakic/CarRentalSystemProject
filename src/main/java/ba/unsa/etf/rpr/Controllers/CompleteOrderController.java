beeecf31502ff8be4aca76c2f47b75b062232c9b
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-11 13:01:21 +0100

    Added new options

--- src/main/java/ba/unsa/etf/rpr/Controllers/CompleteOrderController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/CompleteOrderController.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import static ba.unsa.etf.rpr.Controllers.OrderController.automobil;
+
 public class CompleteOrderController {
+
+    public TextField MarkaTextField;
+    public TextField ModelTextField;
+    public TextField GodisteTextField;
+
+    @FXML
+    public void initialize() {
+    MarkaTextField.setEditable(false);
+    ModelTextField.setEditable(false);
+    GodisteTextField.setEditable(false);
+    MarkaTextField.setText(automobil.getMake());
+    ModelTextField.setText(automobil.getModel());
+    GodisteTextField.setText(String.valueOf(automobil.getCarYear()));
+    }
+
 }
