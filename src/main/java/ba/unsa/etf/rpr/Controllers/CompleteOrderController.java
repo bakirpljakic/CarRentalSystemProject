c9f4fe7be7db75e9c9597b712cc8e692fce5fc88
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 00:49:11 +0100

    Added new options

--- src/main/java/ba/unsa/etf/rpr/Controllers/CompleteOrderController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/CompleteOrderController.java
@@ -1,7 +1,17 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.business.CarsManager;
+import ba.unsa.etf.rpr.business.OrdersManager;
+import ba.unsa.etf.rpr.domain.Orders;
+import ba.unsa.etf.rpr.exceptions.CarsException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+
+import java.sql.Date;
+
+import static ba.unsa.etf.rpr.Controllers.LoginController.korisnik;
 import static ba.unsa.etf.rpr.Controllers.OrderController.automobil;
 
 public class CompleteOrderController {
@@ -10,14 +20,34 @@ public class CompleteOrderController {
     public TextField ModelTextField;
     public TextField GodisteTextField;
 
+    public DatePicker DatumID;
+    public TextField CijenaTextField;
+
+    OrdersManager ordersManager = new OrdersManager();
+
     @FXML
     public void initialize() {
-    MarkaTextField.setEditable(false);
-    ModelTextField.setEditable(false);
-    GodisteTextField.setEditable(false);
-    MarkaTextField.setText(automobil.getMake());
-    ModelTextField.setText(automobil.getModel());
-    GodisteTextField.setText(String.valueOf(automobil.getCarYear()));
+        MarkaTextField.setEditable(false);
+        ModelTextField.setEditable(false);
+        GodisteTextField.setEditable(false);
+        CijenaTextField.setEditable(false);
+        MarkaTextField.setText(automobil.getMake());
+        ModelTextField.setText(automobil.getModel());
+        GodisteTextField.setText(String.valueOf(automobil.getCarYear()));
+        CijenaTextField.setText(String.valueOf(automobil.getPrice()));
     }
 
+    CarsManager carsManager = new CarsManager();
+    public void KupiButton(ActionEvent actionEvent) throws CarsException {
+        Orders o = new Orders();
+        Date date = Date.valueOf(DatumID.getValue());
+        int cijena = Integer.parseInt(CijenaTextField.getText());
+        o = new Orders(0, date, cijena, automobil, korisnik);
+        ordersManager.add(o);
+        carsManager.delete(automobil.getId());
+
+    }
+
+    public void OdjavaButton(ActionEvent actionEvent) {
+    }
 }
