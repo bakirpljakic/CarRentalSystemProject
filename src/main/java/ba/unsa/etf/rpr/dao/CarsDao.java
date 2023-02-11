7fdc285d2b0d6ec0a872085c972bc71d86574295
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-11 18:51:30 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/dao/CarsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarsDao.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Cars;
+import ba.unsa.etf.rpr.exceptions.CarsException;
 
 import java.util.List;
 
 public interface CarsDao extends Dao<Cars>{
 
-    List<Cars> getAllAvailable();
+    List<Cars> getAllAvailable() throws CarsException;
+
+    //int getID(String marka, String model, int godiste, int cijena, boolean b);
 }
