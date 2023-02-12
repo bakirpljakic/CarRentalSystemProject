f852a786e873c80943a7f69440cce165add6b338
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 22:59:55 +0100

    Generated javadoc

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -1,7 +1,20 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * The interface Idable.
+ */
 public interface Idable {
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     int getId();
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     void setId(int id);
 }
