a20092cd6c3a91e2cf5414c0a5547924666cd28e
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 22:59:21 +0100

    Generated javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,18 +1,36 @@
 package ba.unsa.etf.rpr.dao;
 
+/**
+ * The type Dao factory.
+ */
 public class DaoFactory {
     private static final CarsDao carsDao = CarsDaoSQLImpl.getInstance();
     private static final CustomersDao customersDao = CustomersDaoSQLImpl.getInstance();
     private static final OrdersDao ordersDao = OrdersDaoSQLImpl.getInstance();
 
+    /**
+     * Cars dao cars dao.
+     *
+     * @return the cars dao
+     */
     public static CarsDao carsDao() {
         return carsDao;
     }
 
+    /**
+     * Customers dao customers dao.
+     *
+     * @return the customers dao
+     */
     public static CustomersDao customersDao() {
         return customersDao;
     }
 
+    /**
+     * Orders dao orders dao.
+     *
+     * @return the orders dao
+     */
     public static OrdersDao ordersDao() {
         return ordersDao;
     }
