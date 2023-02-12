72e82e33497f345bf7e726bea686175ccecbcb02
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 01:30:46 +0100

    Added validation in LoginController

--- src/main/java/ba/unsa/etf/rpr/Controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/LoginController.java
@@ -8,10 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,57 +20,55 @@ import java.util.List;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class LoginController {
+    public static Customers korisnik = new Customers();
     public TextField KorisnickoimeID;
     public PasswordField LozinkaID;
-
+    public Text upozorenje;
+    public Label welcomeText;
+    Stage stage = new Stage();
+    Customers c = new Customers();
+    private CustomersManager cmanager = new CustomersManager();
     public LoginController() {
     }
 
-    public Label welcomeText;
-
     @FXML
-    public void initialize() {}
-
-    Stage stage = new Stage();
-    private CustomersManager cmanager = new CustomersManager();
-
-
+    public void initialize() {
+    }
 
     public void buttonClick(javafx.event.ActionEvent actionEvent) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/registration.fxml"));
-       // Registracija r = fxmlLoader.getController();
-        //Parent root = fxmlLoader.load();
         RegistrationController r = new RegistrationController();
         fxmlLoader.setController(r);
         Scene scene = new Scene((Parent) fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+
         //stage.setTitle("");
         //stage.setFullScreen(true);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-        stage.setOnHiding(x->{
+        stage.setOnHiding(x -> {
             List<String> lista = r.login();
             KorisnickoimeID.setText(lista.get(0));
             LozinkaID.setText(lista.get(1));
         });
     }
 
-    Customers c = new Customers();
-
     public void PrijavaButtonClick(ActionEvent actionEvent) throws IOException, CarsException {
-       // c = (Customers) cmanager.getAll();
-       c = cmanager.getLoggedInCustomer(KorisnickoimeID.getText(), LozinkaID.getText());
-
-        if(c == null){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Neuspjesna prijava");
-            alert.setHeaderText("Pogrešni podaci!");
-            alert.setContentText("Uneseni su pogrešni podaci.");
-            alert.showAndWait();
-        }
-        else{
-            if(c.isAdmin()){
+        c = cmanager.getLoggedInCustomer(KorisnickoimeID.getText(), LozinkaID.getText());
+        if (c == null) {
+            if (KorisnickoimeID.getText().isBlank() && LozinkaID.getText().isBlank()) {
+                upozorenje.setText("Unesite Vaše korisničko ime i lozinku.");
+            } else if (KorisnickoimeID.getText().isBlank() && !LozinkaID.getText().isBlank()) {
+                upozorenje.setText("Unesite Vaše korisničko ime.");
+            } else if (!KorisnickoimeID.getText().isBlank() && LozinkaID.getText().isBlank()) {
+                upozorenje.setText("Unesite Vašu lozinku.");
+            } else {
+                upozorenje.setText("Unesite ispravno korisničko ime i lozinku!");
+            }
+        } else {
+            if (c.isAdmin()) {
+                korisnik = c;
                 Stage stage = (Stage) KorisnickoimeID.getScene().getWindow();
                 stage.close();
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/cars.fxml"));
@@ -79,7 +77,9 @@ public class LoginController {
                 stage.setTitle("Automobili");
                 stage.setScene(scene);
                 stage.show();
-            } else{
+
+            } else {
+                korisnik = c;
                 Stage stage = (Stage) KorisnickoimeID.getScene().getWindow();
                 stage.close();
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/order.fxml"));
