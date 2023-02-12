30c195443cf7045b679ae73195c12acc4dbe82c9
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 22:58:06 +0100

    Generated javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/CustomersDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CustomersDao.java
@@ -3,11 +3,28 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Customers;
 import ba.unsa.etf.rpr.exceptions.CarsException;
 
+/**
+ * The interface Customers dao.
+ */
 public interface CustomersDao extends Dao<Customers>{
     //public Customers addCustomer(Customers customer);
 
+    /**
+     * Gets logged in customer.
+     *
+     * @param username the username
+     * @param password the password
+     * @return the logged in customer
+     * @throws CarsException the cars exception
+     */
     public Customers getLoggedInCustomer(String username, String password) throws CarsException;
 
+    /**
+     * Provjeri korisnika boolean.
+     *
+     * @param username the username
+     * @return the boolean
+     */
     public boolean provjeriKorisnika(String username);
 
 }
