6941887c3f64c5966208c302bf47d6a6079439d9
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 22:59:30 +0100

    Generated javadoc

--- src/main/java/ba/unsa/etf/rpr/business/OrdersManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/OrdersManager.java
@@ -6,8 +6,18 @@ import ba.unsa.etf.rpr.exceptions.CarsException;
 
 import java.util.List;
 
+/**
+ * The type Orders manager.
+ */
 public class OrdersManager {
 
+    /**
+     * Add orders.
+     *
+     * @param o the o
+     * @return the orders
+     * @throws CarsException the cars exception
+     */
     public Orders add(Orders o) throws CarsException {
         try {
             return DaoFactory.ordersDao().add(o);
@@ -15,6 +25,13 @@ public class OrdersManager {
             throw new CarsException(e.getMessage(), e);
         }
     }
+
+    /**
+     * Delete.
+     *
+     * @param orderId the order id
+     * @throws CarsException the cars exception
+     */
     public void delete(int orderId) throws CarsException {
         try {
             DaoFactory.ordersDao().delete(orderId);
@@ -22,13 +39,35 @@ public class OrdersManager {
             throw new CarsException(e.getMessage(), e);
         }
     }
-        public Orders update(Orders o) throws CarsException {
+
+    /**
+     * Update orders.
+     *
+     * @param o the o
+     * @return the orders
+     * @throws CarsException the cars exception
+     */
+    public Orders update(Orders o) throws CarsException {
             return DaoFactory.ordersDao().update(o);
         }
 
+    /**
+     * Gets all.
+     *
+     * @return the all
+     * @throws CarsException the cars exception
+     */
     public List<Orders> getAll() throws CarsException {
         return DaoFactory.ordersDao().getAll();
     }
+
+    /**
+     * Gets by id.
+     *
+     * @param id the id
+     * @return the by id
+     * @throws CarsException the cars exception
+     */
     public Orders getById(int id) throws CarsException {
         return DaoFactory.ordersDao().getById(id);
     }
