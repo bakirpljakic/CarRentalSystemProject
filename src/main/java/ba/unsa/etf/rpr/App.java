5c5682d8e787ce4ee9b40f49f91d9f5d894e6598
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 02:58:25 +0100

    Added new options

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,14 +1,11 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.Controllers.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.awt.*;
-import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -18,14 +15,24 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
        /* Controller controller = new Controller();
         fxmlLoader.setController(controller);*/
+        String css = String.valueOf(this.getClass().getResource("/css/login.css"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
-        stage.setTitle("LOGIN");
+        scene.getStylesheets().add(css);
+        stage.getIcons().add(new Image("/img/sale_car.jpg"));
+        stage.setTitle("");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
     public static void main(String[] args) {
+      /*  Customers cus = new Customers(0, "hahah", "hahah","hahah","hahah","hahah");
+        CustomersDao cusDao = new CustomersDaoSQLImpl();
+        Customers c = new Customers();
+        c = cusDao.addCustomer(cus);*/
         launch();
+
+
+
     }
 }
