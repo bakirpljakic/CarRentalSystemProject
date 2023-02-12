bb9bcd919e60bc8fde327160b36531bd88c415df
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-12 22:57:03 +0100

    Generated javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/CarsDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarsDao.java
@@ -5,10 +5,30 @@ import ba.unsa.etf.rpr.exceptions.CarsException;
 
 import java.util.List;
 
+/**
+ * The interface Cars dao.
+ */
 public interface CarsDao extends Dao<Cars>{
 
+    /**
+     * Gets all available.
+     *
+     * @return the all available
+     * @throws CarsException the cars exception
+     */
     List<Cars> getAllAvailable() throws CarsException;
 
-    //int getID(String marka, String model, int godiste, int cijena, boolean b);
+    /**
+     * Gets id.
+     *
+     * @param model    the model
+     * @param marka    the marka
+     * @param godiste  the godiste
+     * @param cijena   the cijena
+     * @param dostupno the dostupno
+     * @return the id
+     * @throws CarsException the cars exception
+     */
+//int getID(String marka, String model, int godiste, int cijena, boolean b);
     int getID(String model, String marka, Integer godiste,Integer cijena, boolean dostupno) throws CarsException;
 }
