77fce8f2b7ac34ea4385bd9f7ae30805b2f484cc
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2023-02-13 01:38:29 +0100

    Added CustomersManagerTest

---
+++ src/test/java/ba/unsa/etf/rpr/business/CustomersManagerTest.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomersManagerTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getLoggedInCustomer() {
+    }
+}
